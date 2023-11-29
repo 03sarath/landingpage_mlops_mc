062f06322e9cb46421f8d503a3d4413886cdb8e3
Sarath kumar <03sarath@gmail.com>
2023-11-29 12:12:06 +0530

    Added Amplify details

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      overrideEndpoint: false,
+      region: 'ap-south-1',
+      bucket: 'mlops.psitrontech.com',
+      createBucket: true,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'error.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5
+    }
+  }
 })
